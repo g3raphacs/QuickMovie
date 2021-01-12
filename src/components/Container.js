43600c4fdcd1341f3b2a7da6668a6565d26de6b0
g3raphacs <g3raphacs@gmail.com>
2021-01-12 15:07:29 +0100

    Container + Stars components

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,10 +2,12 @@ import React from 'react';
 import FontAwesome from 'react-fontawesome';
 
 const Container = props => {
+    return(
     <div className ="container">
         <FontAwesome name={props.iconName} size='5x'/>
         <h3 className="container--title">{props.content}</h3>
     </div>
+    )
 }
 
 export { Container }
---
+++ src/components/Stars.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import FontAwesome from 'react-fontawesome';
+
+const Stars = props => {
+    renderStars1 = () => {
+        return props.fakeArray1.map((element, i) => {
+            return(
+                <FontAwesome
+                    key={i}
+                    className="Stars"
+                    name="star"
+                    size="3x"
+                />
+            )
+        })
+    }
+    renderStars2 = () => {
+        return props.fakeArray2.map((element, i) => {
+            return(
+                <FontAwesome
+                    key={i}
+                    className="Stars"
+                    name="star-o"
+                    size="3x"
+                />
+            )
+        })
+    }
+    return(
+        <div className="stars">
+            {renderStars1()}
+            {renderStars2()}
+        </div>
+    )
+}
+
+export { Stars }
